08c945fdce86e4c1dc83ead46bfacf6ffc434dc8
wahyuhidayat1911 <whyuhidayat1911@gmail.com>
2023-08-09 15:09:39 +0700

    try fetch data

--- script/script.js
+++ script/script.js
@@ -1,4 +1,13 @@
 alertButton = document.getElementById("alert-button");
 alertButton.addEventListener("click", () => {
-  alert("ok");
+  alert("get data");
+  logMovies();
 });
+
+async function logMovies() {
+  const response = await fetch(
+    "https://my-json-server.typicode.com/typicode/demo/posts"
+  );
+  const movies = await response.json();
+  console.log(movies);
+}
